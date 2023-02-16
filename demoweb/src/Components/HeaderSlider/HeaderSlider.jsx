6714021a32aa67f157132f02e73c45e9a12d9b67
unknown <ahxanimran456@gmail.com>
2023-02-16 12:05:01 +0500

    create projects and components

---
+++ demoweb/src/Components/HeaderSlider/HeaderSlider.jsx
@@ -0,0 +1,15 @@
+import { MailOutlined } from '@ant-design/icons';
+
+function HeaderSlider() {
+    return (
+        <div className="topheader">
+            <div>
+                <MailOutlined />
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+
+export default HeaderSlider;
---
+++ demoweb/src/Screens/Home/Home.jsx
@@ -0,0 +1,12 @@
+import HeaderSlider from "../../Components/HeaderSlider/HeaderSlider";
+function Home() {
+    return (
+        <div className="container">
+            <div>
+                <HeaderSlider />
+            </div>
+        </div>
+    );
+}
+
+export default Home;
