9552c05d263519086704040f22f2bd1d9c5be6dc
unknown <ahxanimran456@gmail.com>
2023-02-16 19:14:36 +0500

    UPDATE

--- demoweb/src/Components/Service/Services.jsx
+++ demoweb/src/Components/Service/Services.jsx
@@ -9,7 +9,77 @@ function Services() {
                             Our Best Services For You
                         </h3>
                     </div>
-                    .ourservice-prodcts
+                    {/* <div className="ourservice-prodcts">
+
+                    </div> */}
+                    <div className="row servicessec">
+                        <div className="col-md-3 col-sm-12">
+                            <div className='viewcart'>
+                                <img src={require("../../Assets/Images/background1.jpg")} alt="" />
+                            </div>
+                            <div>
+                                <h3>PARTY HALLS</h3>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-12">
+                            <div className='viewcart'>
+                                <img src={require("../../Assets/Images/person.jpg")} alt="" />
+                            </div>
+                            <div>
+                                <h3>SALOONS</h3>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-12">
+                            <div className='viewcart'>
+                                <img src={require("../../Assets/Images/catring.jpg")} alt="" />
+                            </div>
+                            <div>
+                                <h3>CATRING SERVICES</h3>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-12">
+                            <div className='viewcart'>
+                                <img src={require("../../Assets/Images/dress design.jpg")} alt="" />
+                            </div>
+                            <div>
+                                <h3>DRESS DESIGERS</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-3 col-sm-12">
+                            <div className='viewcart'>
+                                <img src={require("../../Assets/Images/orginatization.jpg")} alt="" />
+                            </div>
+                            <div>
+                                <h3>EVENT ORGANIZERS</h3>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-12">
+                            <div className='viewcart'>
+                                <img src={require("../../Assets/Images/background3.jpg")} alt="" />
+                            </div>
+                            <div>
+                                <h3>STAGE DECORATION</h3>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-12">
+                            <div className='viewcart'>
+                                <img src={require("../../Assets/Images/background2.jpg")} alt="" />
+                            </div>
+                            <div>
+                                <h3>LIGHTING DECORATION</h3>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-12">
+                            <div className='viewcart'>
+                                <img src={require("../../Assets/Images/rentacar.jpg")} alt="" />
+                            </div>
+                            <div>
+                                <h3>RENT A CAR </h3>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
